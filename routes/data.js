8142bef0162c83445117977957eb59027e37d6d0
DinaDinakaran <dinadinakaran143@gmail.com>
2022-08-23 18:03:04 +0530

    last

--- routes/data.js
+++ routes/data.js
@@ -6,38 +6,15 @@ const data =require ("../data.json");
 
 
 
-// router.get("/:id",(req,res)=>{
-//     const ID = req.params.id
-//     const result = data.filter((data)=>data.id===ID)
-//     res.status(200).send(result)
-// router.post("/create",(req,res)=>{
-//    console.log(req.body)
-//    try {
-// const value = new questionDetails(req.body)
-// value.save((err,data)=>{
-//   if(err){
-//     res.status(400).send(err)
-//     console.log(err)
-//   }
-//   res.status(201).json(data)
-// })
-//    } catch (error) {
-//       console.log(error)
-//    }
-// })
-
-// })
-
-
-
 router.get('/',(req,res)=>{
 res.status(200).json({message:"Welcome to the Quiz App"})
 })
 
 router.get('/:id',(req,res)=>{
    const ID = req.params.id
+   console.log(ID,typeof(ID))
        const result = data.filter((data)=>data.id===ID)
-       res.status(200).send(result)
+       res.status(200).send(result) 
    })
 
 
